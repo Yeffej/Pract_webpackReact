e063b959882891fddeefdfd3b1308e8dd568c2b9
Yefri Encarnación <yefriej128@gmail.com>
2021-06-03 11:10:56 -0400

    [Add][Initialization] webpack configuration and App react component

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const cssMinimizerPlugin = require("css-minimizer-webpack-plugin")
+
+/** @type {import('webpack').Configuration} */
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+        clean: true
+    },
+    mode: "production",
+    resolve: {
+        extensions: [".js", ".jsx"],
+        alias: {
+            "@components": path.resolve(__dirname, "src/Components"),
+            "@styles": path.resolve(__dirname, "src/Styles")
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                   loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    { loader: "html-loader" }
+                ]
+            },
+            {
+                test: /\.s?[ac]ss$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: "./public/index.html",
+            filename: "./index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
+        }) 
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [`...`, new cssMinimizerPlugin()]
+    }
+}
